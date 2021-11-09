f392b187141738d7a5fb6b88a6a1f9ee3fa3870c
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2021-11-09 04:26:53 -0500

    feat, refactor(cesar.h): Added new constants + refactoring✨🔨

--- cesar.h
+++ cesar.h
@@ -26,10 +26,39 @@
 // Provides macros for reporting and retrieving error conditions using the
 // symbol errno
 #include <errno.h>
+//------------------------------------------------------------------------------
+// ☆ Constants declarations ☆
+//------------------------------------------------------------------------------
+#define EXECUTABLE list_of_arguments[0]
+#define INPUT_FILE list_of_arguments[1]
+#define OUTPUT_FILE list_of_arguments[2]
+#define LINE_SEPARATOR                                                         \
+  "==========================================================================" \
+  "======\n"
+#define ERR_MSG_NO_ARGS                                                        \
+  "✗ Error, please insert arguments to the program!…\nEx: ./cesar "        \
+  "file_to_translate file_append_result\n"
+#define ERR_MSG_FILE_EMPTY                                                     \
+  "✗ Error, the file ☞ %s passed to the program is empty!…\n"
+#define ERR_MSG_OUTPUT_FILE_DOESNT_EXIST                                       \
+  "✗ Error, the output file doesn't exist!…\n"
+#define ERR_MSG_DOESNT_EXIST "✗ Error, the input file ☞ %s doesn't exist!…\n"
+#define ERR_MSG_NOT_TRANSLATED                                                 \
+  "✔︎ The translation of %s was NOT writen in the %s!…\nPlease, enter "  \
+  "valid arguments! Ex: ./cesar input.txt output.txt\n"
+#define ERR_MSG_END "❥ End of the program!…\n"
+#define MSG_WELCOME                                                            \
+  "★ Welcome, this program will translate a file encrypted with Cesar "      \
+  "cypher!\n"
+#define MSG_SUCCESS "✔︎ Success, the result is the follow ⬇︎\n"
+#define MSG_IS_TRANSLATED                                                      \
+  "✔︎ The translation of %s was writen in the %s!…\n"
+#define MSG_ORIGINAL_FILE "✦ The original line #%d is :\n"
+#define MSG_TRANSLATED_FILE "✦ The translated line #%d is :\n"
 
-// Constants declarations
-#define EXECUTABLE arguments_list[0]
-#define INPUT_FILE arguments_list[1]
+#define MSG_SIZE "ℹ️ The file contains %d characters!…\n"
+#define MSG_END "❤︎ End of the program!…\n"
+#define MAX_CHARACTERS_FOR_LINE 80
 #define OUTPUT_FILE arguments_list[2]
 #define ERR_MSG_NO_ARGS "✗ Error, please insert arguments to the program!…\n"
 #define ERR_MSG_FILE_EMPTY "✗ Error, the file passed to the program is empty!…\n"
@@ -37,12 +66,13 @@
 #define MSG_TRANSLATED_FILE "✦ The translated file is :\n"
 #define ALPHABET_NUMBER 26
 //------------------------------------------------------------------------------
-// Declarations of ∀types with docstrings
+// ☆ ∀ TYPES DECLARATIONS ☆
 //------------------------------------------------------------------------------
 enum Error {
   OK,
   ERR_NO_ARGS,
   ERR_FILE_EMPTY,
+  ERR_FILE_DOESNT_EXIST,
 };
 //------------------------------------------------------------------------------
 // ∀functions prototypes (declarations)
