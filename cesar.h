1dd2078c47bdf9e1a43fa7e2bf02a724dedf56b0
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2021-11-09 04:28:55 -0500

    feat(cesar.h): Added functions declarations to header✨

--- cesar.h
+++ cesar.h
@@ -7,10 +7,9 @@
  * @date 2021-10-20
  *
  * @copyright Copyright (c) 2021
- *
  */
 //------------------------------------------------------------------------------
-// Preprocessor tasks
+// ☆ Preprocessor tasks ☆
 //------------------------------------------------------------------------------
 // Provides many standard library functions for file input and output
 #include <stdio.h>
@@ -59,11 +58,6 @@
 #define MSG_SIZE "ℹ️ The file contains %d characters!…\n"
 #define MSG_END "❤︎ End of the program!…\n"
 #define MAX_CHARACTERS_FOR_LINE 80
-#define OUTPUT_FILE arguments_list[2]
-#define ERR_MSG_NO_ARGS "✗ Error, please insert arguments to the program!…\n"
-#define ERR_MSG_FILE_EMPTY "✗ Error, the file passed to the program is empty!…\n"
-#define MSG_ORIGINAL_FILE "✦ The original file is :\n"
-#define MSG_TRANSLATED_FILE "✦ The translated file is :\n"
 #define ALPHABET_NUMBER 26
 //------------------------------------------------------------------------------
 // ☆ ∀ TYPES DECLARATIONS ☆
@@ -75,5 +69,104 @@ enum Error {
   ERR_FILE_DOESNT_EXIST,
 };
 //------------------------------------------------------------------------------
-// ∀functions prototypes (declarations)
+// ☆ ∀ FUNCTIONS PROTOTYPES (declarations) ☆
+//------------------------------------------------------------------------------
+/**
+ * @brief Translate an encrypted file with a shift of +3 (cesar encryption)
+ */
+void translate_cesar_file(FILE *file_input, FILE *file_output, char *line);
+
+/**
+ * @brief Print the content of a string to stdout
+ *
+ * @param line : a string
+ * @param count : the number of a line
+ */
+void print_line(char *line, unsigned int count);
+
+/**
+ * @brief Print a line to separate things on the terminal
+ */
+void print_separator(void);
+
+/**
+ * @brief Print a welcome message for the program execution to stdout
+ */
+void print_welcome_msg(void);
+
+/**
+ * @brief Print a success message to stdout when program execute without any
+ * problem
+ */
+void print_success_msg(void);
+
+/**
+ * @brief Print an end of program message to stdout when there's no problem
+ *
+ * @param input_file
+ * @param output_file
+ */
+void print_end_success_msg(char *input_file, char *output_file);
+
+/**
+ * @brief Print an end of program message to stderr when the input file doesn't
+ * exist
+ *
+ * @param input_file
+ * @param output_file
+ */
+void print_end_err_msg_doesnt_exist(char *input_file, char *output_file);
+
+/**
+ * @brief Print an end of program message to stderr when the input file is empty
+ *
+ * @param input_file
+ * @param output_file
+ */
+void print_end_err_msg_empty(char *input_file, char *output_file);
+
+/**
+ * @brief Delete all '\n' (line jump) from a string and replace it for a white
+ * space
+ *
+ * @param line : a string
+ */
+void destroy_line_jump(char *line);
+
+/**
+ * @brief Validate if a file is empty
+ *
+ * @param file
+ * @return true : if empty
+ * @return false : if not empty
+ */
+bool file_is_empty(char *file);
+
+/**
+ * @brief Validate if a character is a letter (including capital &
+ * non-capital)
+ *
+ * @param character
+ * @return true : if the character is a letter
+ * @return false  : if the character is not a letter
+ */
+bool is_letter(char character);
+
+/**
+ * @brief Translate a line of 80 characters of a cesar encrypted file
+ *
+ * @param buffer
+ * @return char* : the array of characters translated
+ */
+char *translate_cesar_line(char buffer[]);
+
+/**
+ * @brief Return the size of a file
+ *
+ * @param file_name
+ * @return int : the size
+ */
+unsigned int get_size_file(char *file_name);
+//------------------------------------------------------------------------------
+// ☆ END OF THE HEADER ☆
 //------------------------------------------------------------------------------
